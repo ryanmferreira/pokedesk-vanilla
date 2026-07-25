4edad0e5549777e8ba0f44c7a37457408e1735fc
ryanmferreira <dev.ryanmferreira@outlook.com>
2026-07-25 17:42:43 -0300

    Implement Firebase authentication: add login functionality with Google and restructure related files

---
+++ js/database/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9S4937lRGwO8Cok56BwbA2GBtKRvfAM8",
+    authDomain: "pokedesk-vanilla.firebaseapp.com",
+    databaseURL: "https://pokedesk-vanilla-default-rtdb.firebaseio.com",
+    projectId: "pokedesk-vanilla",
+    storageBucket: "pokedesk-vanilla.firebasestorage.app",
+    messagingSenderId: "514055994237",
+    appId: "1:514055994237:web:23f87d602b4d6f99fd6a14"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getDatabase(app);
+export { signInWithPopup };
